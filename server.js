49e567e09addb0321b97da26525f90cab32a6d7a
Glitch (pumped-meteor-snowflake) <none>
2020-09-09 15:09:22 +0000

    Rewound to commit 6114ae1d7c7c4150be1187ee424efe4e9c6f0c14 + 0 edits

--- server.js
+++ server.js
@@ -883,27 +883,6 @@ class io_fastspin extends IO {
     };
   }
 }
-class io_superspin extends IO {
-    constructor(b) {
-        super(b);
-        this.a = 0;
-    }
-    
-    think(input) {
-        this.a += 120;
-        let offset = 0;
-        if (this.body.bond != null) {
-            offset = this.body.bound.angle;
-        }
-        return {                
-            target: {
-                x: Math.cos(this.a + offset),
-                y: Math.sin(this.a + offset),
-            },  
-            main: true,
-        };        
-    }
-}
 class io_reversespin extends IO {
   constructor(b) {
     super(b);
@@ -5612,7 +5591,7 @@ var maintainloop = (() => {
         begin = "yo some shit is about to move to a lower position",
         arrival =
           "Something happened lol u should probably let Neph know this broke",
-        loc = "bas1"/*"norm"*/;
+        loc = "norm";
       let spawn = () => {
         let spot,
           m = 0;
@@ -5733,27 +5712,10 @@ case 4:
     // Make base protectors if needed.
     let f = (loc, team) => {
       let o = new Entity(loc);
-      o.define(Class.overseer);
+      o.define(Class.levelo);
       o.team = -team;
       o.color = [10, 11, 12, 15][team - 1];
     };
-/*  // ======================================
-          // MODIFIED: Respawning Base Protectors.
-          // ======================================
-          o.ondead = () => {
-            let n = new Entity(loc);
-            n.define(Class.baseProtector);
-                n.team = -team;
-                n.color = [10, 12, 11, 15][team-1];
-            n.ondead = o.ondead
-            o = n
-        }
-        // ======================================
-          
-        };
-        for (let i=1; i<5; i++) {
-            room['bas' + i].forEach((loc) => { f(loc, i); }); 
-        }*/
     for (let i = 1; i < 5; i++) {
       room["bas" + i].forEach(loc => {
         f(loc, i);
